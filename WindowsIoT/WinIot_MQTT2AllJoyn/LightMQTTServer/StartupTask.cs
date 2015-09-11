1f211efaacd441addc1b4c0f056d5e2f9c4de5df
QianFan Jiang <bucherjiang@msn.com>
2015-09-11 12:04:58 +0800

    initial checkin for MQTT server

---
+++ WindowsIoT/WinIot_MQTT2AllJoyn/LightMQTTServer/StartupTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using uPLibrary.Networking.M2Mqtt;
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace LightMQTTServer
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral deferral;
+        MqttBroker broker;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to start one or more asynchronous methods 
+            //
+            broker = new MqttBroker();
+            deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += TaskInstance_Canceled;
+            broker.UserAuth = (name, pwd) => 
+            {
+                return true;//every one can access
+            };
+            broker.Start();
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            broker?.Stop();
+            deferral.Complete();
+        }
+    }
+}
